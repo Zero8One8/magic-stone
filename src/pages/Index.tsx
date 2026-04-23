87d6eee4a517a744e1984de9c39807f5f6e05bc6
Zero8One8 <9689388368one@gmail.com>
2026-04-23 23:40:46 +0300

    fix_freekassa_static_banner_code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -17,12 +17,7 @@ const Index = () => {
       <LeadMagnetSection />
       <PhilosophySection />
       <ChannelSection />
-      
-      {/* Freekassa payment provider */}
-      <div className="flex justify-center items-center py-8 px-6">
-        <a href="https://freekassa.net" target="_blank" rel="noopener noreferrer"><img src="https://freekassa.net/img/logo/freekassa.png" alt="Freekassa - прием платежей" border="0" title="Freekassa" /></a>
-      </div>
-      
+
       <ClosingSection />
     </main>
   );
